9db8a0f1775712c4a9c54b33c05fa405beda206b
bobbytg <32232327+bobbytg@users.noreply.github.com>
2024-10-06 08:18:11 +0000

    T787

--- views/about_page.py
+++ views/about_page.py
@@ -14,15 +14,44 @@ with col2:
         show_contact_form()
 
 
-# --- EXPERIENCE & QUALIFICATIONS ---
+# --- About ---
 st.write("\n")
 st.subheader("About Jaidee Dietetic Recipe Generator", anchor=False)
 st.write(
     """
-    - 7 Years experience extracting actionable insights from data
-    - Strong hands-on experience and knowledge in Python and Excel
-    - Good understanding of statistical principles and their respective applications
-    - Excellent team-player and displaying a strong sense of initiative on tasks
+ The JAIDEE Recipe Generator is an intelligent, interactive web application 
+ designed to generate personalized, creative, and user-friendly recipes tailored to individual preferences, 
+ dietary needs and more. 
+
+ Using Streamlit for its sleek user interface and Gemini AI for powerful natural language processing, 
+ this app takes culinary experiences to the next level by offering recipe suggestions 
+ based on a variety of input factors, including ingredients.
     """
 )
 
+# --- SKILLS ---
+st.write("\n")
+st.subheader("User Interaction Flow", anchor=False)
+st.write(
+    """
+1. User Input: Users provide their preferences (cuisine, ingredients, dietary preferences, etc.) 
+   using intuitive dropdowns, text inputs, and buttons.
+
+2. AI Recipe Generation: Based on the input, a detailed prompt is constructed and sent to Gemini AI,
+   which generates a customized recipe in short time.
+
+3. Display Recipe: The app displays the recipe along with additional features like preparation time, 
+   cooking instructions, wine pairing, and nutritional information.
+
+4. Chat Interface: Users can interact with the AI in a conversational way, asking for clarification, 
+   additional recipes, or substitutions.
+    """
+)
+
+# --- SKILLS ---
+st.write("\n")
+st.subheader("POWERED BY MADT", anchor=False)
+st.write(
+    """🚀
+    """
+)
