5ed6579d792f88b98f2d339ff2e5fec710f717b0
bobbytg <32232327+bobbytg@users.noreply.github.com>
2024-10-06 13:20:58 +0000

    s

--- views/jaidee.py
+++ views/jaidee.py
@@ -91,7 +91,7 @@ and the recipe title at the beginning of the response.
 Then include the wine pairing for each recommendation.
 At the end of the recommendation provide the calories associated with the meal
 Then the detalied nutritional facts \n
-Then prepare and is suitable for someone with {skill_level} cooking skills\n
+Then prepare the recepe that suitable for someone with {skill_level} cooking skills\n
 And Please provide the recipe in {language}.
 """
 
